789e6810430cfb92fe99c34039efd4bf6315d898
Egil Hasting <egil.hasting@higen.org>
2017-04-03 20:12:51 +0200

    34234

---
+++ src/fightcalculator.cpp
@@ -0,0 +1,6 @@
+#include "fightcalculator.h"
+
+FightCalculator::FightCalculator()
+{
+
+}
---
+++ src/fightcalculator.h
@@ -0,0 +1,11 @@
+#ifndef FIGHTCALCULATOR_H
+#define FIGHTCALCULATOR_H
+
+
+class FightCalculator
+{
+public:
+    FightCalculator();
+};
+
+#endif // FIGHTCALCULATOR_H
